fdddee75a88fbbfa4eb2abf21dcee620fd3a54c6
clarkeadg <clarkeadg@yahoo.com>
2023-12-18 21:03:32 -0800

    tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,9 +10,13 @@ describe('Renders App component correctly', async () => {
   );
 
   const main = container.querySelector('main');
+  const title = container.querySelector('h1');
   
   it('Should have a main tag', async () => {  
     expect(main).not.toBeNull();
   });
 
+  it('Should have an h1 tag', async () => {  
+    expect(title).not.toBeNull();
+  });
 });
